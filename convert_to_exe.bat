88cf7f2a8893fa5b8d26ecb505626693b75546bc
Aleksander Kupiec <olokup@gmail.com>
2021-01-21 10:52:46 +0100

    konwertowanie do EXE z python w wersji 3.8

--- convert_to_exe.bat
+++ convert_to_exe.bat
@@ -1,1 +1,3 @@
+
+set PATH=C:\Users\olek.SIP\AppData\Local\Programs\Python\Python38\;C:\Users\olek.SIP\AppData\Local\Programs\Python\Python38\\scripts
 pyinstaller --noconfirm --onefile --console  "dovista2cutter.py"
