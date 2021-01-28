0b3f595a21936e0aa986b4c82d9460c2a2cb2b61
Aleksander Kupiec <olokup@gmail.com>
2021-01-28 16:29:06 +0100

    wyłączenie opcjo --onefile

--- convert_to_exe.bat
+++ convert_to_exe.bat
@@ -1,3 +1,3 @@
 
 set PATH=C:\Users\olek.SIP\AppData\Local\Programs\Python\Python38\;C:\Users\olek.SIP\AppData\Local\Programs\Python\Python38\\scripts
-pyinstaller --noconfirm --onefile --console  "dovista2cutter.py"
+pyinstaller --noconfirm --console  "dovista2cutter.py"
