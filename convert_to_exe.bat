33a2e74fcb2426faff5d2d106e0f496737b112d0
Aleksander Kupiec <olokup@gmail.com>
2021-09-13 16:05:44 +0200

    wersja IGP

--- convert_to_exe.bat
+++ convert_to_exe.bat
@@ -1,3 +1,3 @@
 
 set PATH=C:\Users\olek.SIP\AppData\Local\Programs\Python\Python38\;C:\Users\olek.SIP\AppData\Local\Programs\Python\Python38\\scripts
-pyinstaller --noconfirm --console  "dovista2cutter.py"
+pyinstaller --noconfirm --console  --onefile "dovista2cutter.py"
