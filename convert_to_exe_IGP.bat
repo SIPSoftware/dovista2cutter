d8d84370cf10f94205e40dc73b5244ff888cbf2d
Aleksander Kupiec <olokup@gmail.com>
2024-01-18 00:20:33 +0100

    nowy skrypt do kompilacji exe

--- convert_to_exe_IGP.bat
+++ convert_to_exe_IGP.bat
@@ -1,3 +1,3 @@
 
-set PATH=C:\Users\olek.SIP\AppData\Local\Programs\Python\Python38\;C:\Users\olek.SIP\AppData\Local\Programs\Python\Python38\\scripts
-pyinstaller --noconfirm --console  --onefile "dovista2cutterIGP.py"
+::set PATH=C:\Users\olek.SIP\AppData\Local\Programs\Python\Python38\;C:\Users\olek.SIP\AppData\Local\Programs\Python\Python38\\scripts
+call .\3.8\Scripts\activate && pyinstaller --noconfirm --console  --onefile "dovista2cutterIGP.py" && deactivate
