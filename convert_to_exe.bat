b911ce2a2da97a2d85a41bc4f2e94aeb1a23d3df
Aleksander Kupiec <olokup@gmail.com>
2024-02-07 10:48:09 +0100

    poprawa skryptów

--- convert_to_exe.bat
+++ convert_to_exe.bat
@@ -1,3 +1,2 @@
 
-set PATH=C:\Users\olek.SIP\AppData\Local\Programs\Python\Python38\;C:\Users\olek.SIP\AppData\Local\Programs\Python\Python38\\scripts
-pyinstaller --noconfirm --console "dovista2cutter.py"
+call .\3.8\Scripts\activate & pyinstaller --noconfirm --console  --onefile "dovista2cutter.py" & deactivate
--- convert_to_exe_IGP.bat
+++ convert_to_exe_IGP.bat
@@ -1,3 +1,1 @@
-
-::set PATH=C:\Users\olek.SIP\AppData\Local\Programs\Python\Python38\;C:\Users\olek.SIP\AppData\Local\Programs\Python\Python38\\scripts
-call .\3.8\Scripts\activate && pyinstaller --noconfirm --console  --onefile "dovista2cutterIGP.py" && deactivate
+call .\3.8\Scripts\activate & pyinstaller --noconfirm --console  --onefile "dovista2cutterIGP.py" & deactivate
